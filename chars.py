1f8ecb3d6da27679082059dfcfd5b33e8ddb5914
s1tf <119539022+s1tf@users.noreply.github.com>
2022-12-14 17:23:00 +0200

    Add files via upload

---
+++ chars.py
@@ -0,0 +1,1879 @@
+CHARS_RAW = '''
+XXX
+X X
+X X
+XXX
+X X
+X X
+X X
+
+
+
+XXX
+X
+X
+XXX
+X X
+X X
+XXX
+
+
+
+XXX
+X X
+X X
+XXXX
+X  X
+X  X
+XXXX
+
+
+
+XXX
+X
+X
+X
+X
+X
+X
+
+
+
+ XXX
+ X X
+ X X
+ X X
+ X X
+ X X
+XXXXX
+
+
+
+XXX
+X
+X
+XX
+X
+X
+XXX
+
+X X
+
+XXX
+X
+X
+XX
+X
+X
+XXX
+
+
+
+X X X
+X X X
+X X X
+ X X
+X X X
+X X X
+X X X
+
+
+
+XX
+  X
+  X
+XX
+  X
+  X
+XX
+
+
+
+X  X
+X  X
+X  X
+X XX
+XX X
+X  X
+X  X
+
+ XX
+ 
+X  X
+X  X
+X  X
+X XX
+XX X
+X  X
+X  X
+
+
+
+X  X
+X  X
+X X
+XX 
+X X
+X  X
+X  X
+
+
+
+  XX
+ X X
+ X X
+ X X
+ X X
+X  X
+X  X
+
+
+
+X   X
+XX XX
+XX XX
+X X X
+X   X
+X   X
+X   X
+
+
+
+X X
+X X
+X X
+XXX
+X X
+X X
+X X
+
+
+
+XXX
+X X
+X X
+X X
+X X
+X X
+XXX
+
+
+
+XXX
+X X
+X X
+X X
+X X
+X X
+X X
+
+
+
+XXX
+X X
+X X
+XXX
+X
+X
+X
+
+
+
+XXX
+X
+X
+X
+X
+X
+XXX
+
+
+
+XXX
+ X
+ X
+ X
+ X
+ X
+ X
+
+
+
+X X
+X X
+X X
+XXX
+  X
+  X
+XXX
+
+
+ 
+  X
+XXXXX
+X X X
+X X X
+X X X
+XXXXX
+  X
+  
+  
+  
+X X
+X X
+X X
+ X
+X X
+X X
+X X
+
+
+
+X X
+X X
+X X
+X X
+X X
+X X
+XXXX
+   X
+   
+   
+X X
+X X
+X X
+XXX
+  X
+  X
+  X
+  
+
+
+X X X
+X X X
+X X X
+X X X
+X X X
+X X X
+XXXXX
+
+
+
+X X X
+X X X
+X X X
+X X X
+X X X
+X X X
+XXXXXX
+     X
+
+
+XX
+ X
+ X
+ XXX
+ X X
+ X X
+ XXX
+ 
+
+
+X   X
+X   X
+X   X
+XXX X
+X X X
+X X X
+XXX X
+ 
+
+
+X
+X
+X
+XXX
+X X
+X X
+XXX
+
+
+
+XX
+  X
+  X
+XXX
+  X
+  X
+XX
+
+
+
+X  X
+X X X
+X X X
+XXX X
+X X X
+X X X
+X  X
+
+
+
+XXX
+X X
+X X
+XXX
+ XX
+ XX
+X X
+
+
+
+
+
+XXX
+X X
+XXX
+X X
+X X
+
+
+
+
+
+XXX
+X
+XXX
+X X
+XXX
+
+
+
+
+
+XXX
+X X
+XXXX
+X  X
+XXXX
+
+
+
+
+
+XXX
+X
+X
+X
+X
+
+
+
+
+
+ XXX
+ X X
+ X X
+ X X
+XXXXX
+
+
+
+
+
+XXX
+X
+XX
+X
+XXX
+
+
+
+X X
+
+XXX
+X
+XX
+X
+XXX
+
+
+
+
+
+X X X
+X X X
+ X X
+X X X
+X X X
+
+
+
+
+
+XX
+  X
+XX
+  X
+XX
+
+
+
+
+
+X  X
+X  X
+X XX
+XX X
+X  X
+
+
+
+ XX
+
+X  X
+X  X
+X XX
+XX X
+X  X
+
+
+
+
+
+X  X
+X X
+XX
+X X
+X  X
+
+
+
+
+
+  XX
+ X X
+ X X
+X  X
+X  X
+
+
+
+
+
+X   X
+XX XX
+X X X
+X   X
+X   X
+
+
+
+
+
+X X
+X X
+XXX
+X X
+X X
+
+
+
+
+
+XXX
+X X
+X X
+X X
+XXX
+
+
+
+
+
+XXX
+X X
+X X
+X X
+X X
+
+
+
+
+
+XXX
+X X
+XXX
+X
+X
+
+
+
+
+
+XXX
+X
+X
+X
+XXX
+
+
+
+
+
+XXX
+ X
+ X
+ X
+ X
+ 
+ 
+ 
+ 
+
+X X
+X X
+XXX
+  X
+XXX
+
+
+
+
+
+  X
+XXXXX
+X X X
+XXXXX
+  X
+  
+  
+  
+  
+    
+X X
+X X
+ X
+X X
+X X
+
+
+
+
+
+X X
+X X
+X X
+X X
+XXXX
+   X
+  
+
+
+
+X X
+X X
+XXX
+  X
+  X
+  
+
+
+
+
+X X X
+X X X
+X X X
+X X X
+XXXXX
+
+
+
+
+
+X X X
+X X X
+X X X
+X X X
+XXXXXX
+     X
+
+
+
+
+XX
+ X
+ XXX
+ X X
+ XXX
+
+
+
+
+
+X   X
+X   X
+XXX X
+X X X
+XXX X
+
+
+
+
+
+X
+X
+XXX
+X X
+XXX
+
+
+
+
+
+XX
+  X
+XXX
+  X
+XX
+
+
+
+
+
+X XXX
+X X X
+XXX X
+X X X
+X XXX
+
+
+
+
+
+ XXX
+ X X
+ XXX
+ X X
+X  X
+
+
+
+XXX
+X X
+X X
+XXX
+X X
+X X
+X X
+
+
+
+XX
+X X
+X X
+XX
+X X
+X X
+XX
+
+
+
+ XX
+X
+X
+X
+X
+X
+ XX
+
+
+
+XXX
+X  X
+X  X
+X  X
+X  X
+X  X
+XXX
+
+
+
+XXX
+X
+X
+XX
+X
+X
+XXX
+
+
+
+XXX
+X
+X
+XXX
+X
+X
+X
+
+
+
+XXXX
+X
+X
+X XX
+X  X
+X  X
+XXXX
+
+
+
+X X
+X X
+X X
+XXX
+X X
+X X
+X X
+
+
+
+X
+X
+X
+X
+X
+X
+X
+
+
+
+  X
+  X
+  X
+  X
+  X
+X X
+XXX
+
+
+
+X X
+X X
+X X
+XX
+X X
+X X
+X X
+
+
+
+X
+X
+X
+X
+X
+X
+XX
+
+
+
+X   X
+XX XX
+X X X
+X   X
+X   X
+X   X
+X   X
+
+
+
+X  X
+X  X
+XX X
+X XX
+X  X
+X  X
+X  X
+
+
+
+XXX
+X X
+X X
+X X
+X X
+X X
+XXX
+
+
+
+XXX
+X X
+X X
+XXX
+X
+X
+X
+
+
+
+XXX
+X X
+X X
+X X
+X X
+X X
+XXXX
+  
+
+
+XXX
+X X
+X X
+XXX
+XX
+X X
+X X
+
+
+
+XX
+X
+X
+XX
+ X
+ X
+XX
+
+
+
+XXX
+ X
+ X
+ X
+ X
+ X
+ X
+
+
+
+X X
+X X
+X X
+X X
+X X
+X X
+XXX
+
+
+
+X X
+X X
+X X
+X X
+X X
+X X
+ X
+
+
+
+X   X
+X   X
+X   X
+X X X 
+X X X
+X X X
+ X X
+
+
+
+X X
+X X
+X X
+ X
+X X
+X X
+X X
+
+
+
+X X
+X X
+X X
+X X
+ X
+ X
+ X
+
+
+
+XXX
+  X 
+  X
+ X
+X
+X
+XXX
+
+
+
+
+
+XXX
+X X
+XXX
+X X
+X X
+
+
+
+
+X
+XX
+X X
+X X
+X X
+XX
+
+
+
+
+
+ XX
+X
+X
+X
+ XX
+
+
+
+
+   
+XX
+X X
+X X
+X X
+XX
+
+
+
+
+
+XXX
+X
+XXX
+X
+XXX
+
+
+
+
+
+XXX
+X
+XXX
+X
+X
+
+
+
+
+
+XXXX
+X
+X XX
+X  X
+XXXX
+
+
+
+
+
+X X
+X X
+XXX
+X X
+X X
+
+
+
+X
+
+X
+X
+X
+X
+X
+
+
+
+ X
+
+ X
+ X
+ X
+ X
+ X
+X 
+
+
+
+
+X X
+X X
+XX
+X X
+X X
+
+
+
+
+
+X
+X
+X
+X
+XX
+
+
+
+
+
+X   X
+XX XX
+X X X
+X   X
+X   X
+
+
+
+
+
+X  X
+XX X
+X XX
+X  X
+X  X
+
+
+
+
+
+XXX
+X X
+X X
+X X
+XXX
+
+
+
+
+
+XX
+X X
+XX
+X
+X
+
+
+
+
+
+ X
+X X
+X X
+X X
+ XX
+  X
+
+
+
+
+XX
+X X
+XX
+XX
+X X
+
+
+
+
+
+XXX
+X
+XXX
+  X
+XXX
+
+  
+
+
+
+XXX
+ X
+ X
+ X
+ X
+
+
+
+
+
+X X
+X X
+X X
+X X
+XXX
+
+
+
+
+
+X X
+X X
+X X
+X X
+ X
+
+
+
+
+
+X   X
+X   X
+X X X
+X X X
+ X X
+
+
+
+
+
+X X
+X X
+ X
+X X
+X X
+
+
+
+
+
+X X
+X X
+X X
+ X
+X
+
+
+
+
+
+XXX
+  X
+ X
+X
+XXX
+
+
+
+ X
+X X 
+X X
+ X
+X X X
+X  X
+ XX X
+
+
+
+
+
+
+
+
+
+XXXXXX
+
+
+
+
+
+
+
+
+
+X X X
+
+
+
+
+
+
+
+
+
+X
+X
+
+
+
+
+
+
+ XXX
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+X
+
+
+
+
+
+
+
+XX
+
+
+
+
+
+
+
+
+ X
+XXX
+ X
+
+
+
+
+ XXX
+X   X
+   X
+  X
+  X
+  
+  X
+
+
+
+X
+X
+X
+X
+X
+
+X
+
+
+
+X X
+X X
+
+
+
+
+
+
+
+
+ X X
+X X
+
+
+
+
+
+
+
+
+ X X
+X X
+
+
+
+
+
+
+
+
+X
+X
+
+
+
+
+
+
+
+
+  X
+ X
+ X
+X
+ X
+ X
+  X
+
+
+
+X
+ X
+ X
+  X
+ X
+ X
+X
+
+
+
+    X
+   X
+  X
+  X
+  X
+ X
+X
+
+
+
+XX
+X
+X
+X
+X
+X
+XX
+
+
+
+XX
+ X
+ X
+ X
+ X
+ X
+XX
+
+
+
+
+ X
+X
+X
+X
+X
+ X
+
+
+
+
+X
+ X
+ X
+ X
+ X
+X
+
+
+
+
+
+X
+
+
+
+X
+
+
+
+
+
+X  X
+  X
+ X
+ X
+X  X
+
+
+
+ XX
+X  X
+X  X
+X  X
+X  X
+X  X
+ XX
+
+
+
+  X
+ XX
+X X
+  X
+  X
+  X
+  X
+
+
+
+ XX
+X  X
+X  X
+  X
+ X
+X
+XXXX
+
+
+
+XX
+  X
+  X
+XX
+  X
+  X
+XX
+
+
+
+X X
+X X
+X X
+XXX
+  X
+  X
+  X
+
+
+
+XXX
+X
+X
+XX
+  X
+  X
+XX
+
+
+
+ XX
+X  
+X
+XXX
+X  X
+X  X
+ XX
+
+
+
+XXX
+  X
+  X
+ X
+X
+X
+X
+
+
+
+ XX
+X  X
+X  X
+ XX
+X  X
+X  X
+ XX
+
+
+
+ XX
+X  X
+X  X
+ XX
+   X
+   X
+ XX
+
+
+
+
+   
+ X X X
+  XXX
+ XXXXX
+  XXX
+ X X X
+
+
+
+
+
+  X  X
+ X  X  
+X  X
+ X  X
+  X  X
+    
+    
+    
+    
+    
+X  X
+ X  X
+  X  X
+ X  X
+X  X 
+     
+     
+     
+     
+     
+  X
+ X
+X
+ X
+  X
+
+
+
+
+
+X
+ X
+  X
+ X
+X
+
+
+
+
+
+
+XXXX
+
+XXXX
+
+
+
+
+
+  X
+ XXX
+X X X
+  X
+  X
+  X
+
+
+
+
+  X
+  X
+  X
+X X X
+ XXX
+  X
+
+
+
+
+  X
+ X
+XXXXXX
+ X
+  X
+
+
+
+
+
+   X
+    X
+XXXXXX
+    X
+   X
+
+
+
+
+X
+X
+X
+X
+X
+X
+X
+
+
+
+ X
+X
+
+
+
+
+
+
+
+
+ XX   XX
+XXXX XXXX
+XXXXXXXXX
+ XXXXXXX
+  XXXXX
+   XXX
+    X
+
+
+
+  XXX
+ X   X
+X X   X
+X  X  X
+X   X X
+ X   X
+  XXX
+
+
+
+XX     XX
+ XX   XX
+  XX XX
+   XXX
+  XX XX
+ XX   XX
+XX     XX
+
+
+
+       XX
+       XX 
+      XX
+ XX   XX
+ XX  XX
+  XX XX
+   XXX
+
+
+
+     X
+    X X 
+   X X X
+   X X X
+  X     X
+  X  X  X
+  XXXXXXX
+  
+
+
+      XXXXX 
+   xXx   X
+     X  X
+XXX X  X
+ X X  XXXXX
+X  XXX
+XXX
+
+
+
+   X
+  XXX
+  XXX
+ XXXXX
+ XXXXX
+XXXXXXX
+XXXXXXX
+
+
+
+X
+XX
+XXX
+XXXX
+XXX
+XX
+X
+
+
+
+   X
+  XX
+ XXX
+XXXX
+ XXX
+  XX
+   X
+
+
+
+ XXX
+X   X
+X   X
+X   X
+ XXXX
+     X
+      X
+       
+       
+
+X  
+XXXXXXXXX
+XXXXXXXX
+    X XX
+     XXX
+      XX
+      XX
+
+
+
+XXX
+XXX
+XXX
+XXX
+XxX
+XXX
+XXX
+
+
+
+    
+ X X
+XXXXX
+ X X
+ X X 
+XXXXX
+ X X
+
+
+
+X  X XX
+X  X XX
+XX X
+X XX XX
+X  X
+X  X
+X  X
+
+
+
+  XXX
+ X   X
+X XXX X
+X X X X
+ X X X
+  XXX
+  XXX
+
+
+
+  X
+   X
+ XX XX
+XXXXXXX
+XXXXXXX
+ XXXXX
+  XXX
+
+
+
+XXXXXX 
+X    X
+X XX X
+X    X
+X XX X
+X    X
+XXXXXx
+
+
+
+XXXXXXXXX
+XX     XX
+X X   X X
+X  X X  X
+X X X X X
+XX     XX
+XXXXXXXXX
+
+
+
+  XXX
+ X   X 
+X  XX X 
+X X X X
+X X X X
+X  X X 
+ X     
+  
+
+
+
+  X
+ XX
+XXX
+ XXX
+ XX
+ X
+
+
+
+
+XXXXXXX
+ X   X
+ X X X
+ X X X
+ X X X
+ XXXXX
+
+
+
+   X 
+  XXX
+ XXXXX
+X XXX X
+  XXX
+  X X
+  X X
+
+
+
+ XXXX
+XXXXXX
+X XX X
+  XX
+   XX
+   XX
+  XX
+
+
+
+
+ XX  X
+X  XX
+ XX  X
+X  XX
+ XX  X
+X  XX
+
+
+
+
+
+
+
+
+
+'''
+
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import text
 import pyxel
+
+health = 100
 
 
 class App:
     def __init__(self):
-        pyxel.init(160, 120)
+        pyxel.init(320, 240)
         self.x = 0
         pyxel.run(self.update, self.draw)
 
@@ -11,8 +14,8 @@ class App:
         self.x = (self.x + 1) % pyxel.width
 
     def draw(self):
-        pyxel.cls(0)
-        pyxel.rect(self.x, 0, 8, 8, 9)
+        pyxel.cls(pyxel.COLOR_BLACK)
+        text.draw(text=f'Здоровье: {health}', x=1, y=0, color=pyxel.COLOR_RED)
 
 
 App()
